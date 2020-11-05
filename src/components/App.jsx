34e418913af4f478ca8e61b14e5d7ff182ceefb2
gramm500 <60506320+gramm500@users.noreply.github.com>
2020-11-05 07:33:21 +0300

    finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return <div />;
+  let [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={() => setCount((PrevState) => count + 1)}>+</button>
+      <button onClick={() => setCount((PrevState) => count - 1)}>-</button>
+    </div>
+  );
 }
 
 export default App;
